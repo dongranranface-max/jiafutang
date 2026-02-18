f5f1ae654e5a6b44221814a5e5d9fc3019dc667f
Dongr <dongr@example.com>
2026-02-18 19:16:43 +0800

    同步删除data.js中国际资讯分类

--- js/data.js
+++ js/data.js
@@ -17,7 +17,7 @@ const COLLECTION_STATUS = {
 };
 
 const NEWS_CATEGORIES = {
-    all: '全部', brand: '嘉孚堂动态', industry: '行业动态', international: '国际资讯'
+    all: '全部', brand: '嘉孚堂动态', industry: '行业动态'
 };
 
 // ============ 数据加载 ============
